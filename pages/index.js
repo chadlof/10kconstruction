d741ad0f598a727ebd2b1e7b606be08ada58135e
chadlof <clofgren@immersiondata.com>
2021-01-31 18:14:00 -0600

    fixed space

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ const PlaceHolder = () => {
             </Head>
             <CenterWrapper>
                 <LogoWrapper>
-                  <Image src='/static/10kLogo.png' width='350px' height='auto'alt='10K logo'/>
+                  <Image src='/static/10kLogo.png' width='350px' height='auto' alt='10K logo'/>
                 </LogoWrapper>
                 <Text> 10K Construction is a full service general contracting company eager to help you with any of your home's needs. We specialize in everything from exterior storm damage insurance claims to full home remodels and everything in between. We service the entire state of Minnesota from your home in the cities to your cabin in northern MN. Residential, Multi-family, or Commercial we have you covered. Our team of experts are trained and certified to provide excellent service with a quick turnaround time due to our industry leading tradesmen in the field. Our length of experience and time in the industry will help guarantee that you will be glad you picked 10K Construction as your general contractor. </Text>
                 <Text> We strive to make sure our customers have the smoothest experience possible as our partner throughout the entire process. By doing so, we know that following through is an important quality. That’s why, when we say we’re going to do something, we do it! Not when we have time or when it's convenient for us but when you need it done. Equally important is that we will not sacrifice any quality along the way. At 10K Construction, we'll turn your dream project into reality.</Text>
@@ -73,3 +73,53 @@ const PlaceHolder = () => {
 }
 
 export default PlaceHolder
+
+
+
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import Image from 'next/image'
+
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+// }));
+
+// export default function ButtonAppBar() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+//             <MenuIcon />
+//           </IconButton>
+//           {/* <Typography variant="h6" className={classes.title}> */}
+//           <Button color="inherit">Login</Button>
+//           <Image
+//             src="/../static/10kLogo.png"
+//             alt="10 K construction logo"
+//             width={70}
+//             height={50}
+//           />
+//           {/* </Typography> */}
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
